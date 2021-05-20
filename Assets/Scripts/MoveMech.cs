fc17963b56ce95f3722ec7293635acfb2cdd0846
HeinzGuderian <mattias.olsson.nu@gmail.com>
2021-05-20 10:34:59 +0200

    Tog bort menu scenen, ska försöka annat sätt utveckla local multiplayer

--- Assets/Scripts/MoveMech.cs
+++ Assets/Scripts/MoveMech.cs
@@ -30,7 +30,7 @@ public class MoveMech : MonoBehaviour {
         ForwardSpeeds = new float[3] { 0, -5f, 5f };
         SidewaysSpeeds = new float[3] { 0, -20f, 20f };
         TurnVector = new Vector3(0f, 0f, 0f);
-        PlayerInput.Instantiate(PilotPrefab, playerIndex: 0, controlScheme: "KeyboardMouse", pairWithDevices: new InputDevice[] { Keyboard.current, Mouse.current });
+        //PlayerInput.Instantiate(PilotPrefab, playerIndex: 0, controlScheme: "KeyboardMouse", pairWithDevices: new InputDevice[] { Keyboard.current, Mouse.current });
     }
 
     void Start()
